a6a2cd1854057c72fe02495af7e3c070bc458671
unknown <kane24051@gmail.com>
2022-05-04 00:04:35 +0300

    lesson_4

---
+++ lesson_4.js
@@ -0,0 +1,26 @@
+// function yolochka(item_1, item_2){
+
+//     let mass = 'Hello!'
+//     console.log(mass, item_1, item_2)
+// }
+
+// yolochka(11, 22)
+
+let t1 = 10
+let t2 = 20
+
+// let f1 = () => {console.log("World")}
+// let f22 = (t1, t2) => {console.log('Sum = ', t11 + t22)}
+
+let t3 = 30
+let t4 = 40
+
+// f1()
+
+// f22()
+
+let f33 = (t1 < t2) ?
+    (tt1, tt2) => console.log('order = ', tt1 + tt2) :
+    (tt1, tt2) => console.log('order = ', tt1 - tt2) ;
+
+f33(t3, t4)
